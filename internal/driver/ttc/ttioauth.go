d13458d346d82a73c0a9f58ee3c34cc7da025d8f
Jean de Lavarene <28743284+jeandelavarene@users.noreply.github.com>
2026-08-26 19:43:08 +0200

    After review with the team, we decided to remove "Thin" from the driver name

--- internal/driver/ttc/ttioauth.go
+++ internal/driver/ttc/ttioauth.go
@@ -90,10 +90,8 @@ const (
 	authClientCapabilities = "AUTH_CLIENT_CAPABILITIES"
 	authPbkdf2VgenCount    = "AUTH_PBKDF2_VGEN_COUNT"
 	authPbkdf2SderCount    = "AUTH_PBKDF2_SDER_COUNT"
-	// driverNameDefault is reported in V$SESSION_CONNECT_INFO.CLIENT_DRIVER.
-	// Then string below is consistent with what Python uses: "python-oracledb Thin"
-	driverNameDefault = "go-oracledb Thin"
-	authOraEdition    = "AUTH_ORA_EDITION"
+	driverNameDefault      = "go-oracledb" // driverNameDefault is reported in V$SESSION_CONNECT_INFO.CLIENT_DRIVER.
+	authOraEdition         = "AUTH_ORA_EDITION"
 
 	passwordBufferLength = 2112
 	kolrugEnable         = 0x0001
